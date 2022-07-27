9ee38a6eeb220a42fb96e8e16507514eee03e172
Istvan <palocsayi@gmail.com>
2022-07-27 20:32:15 +0200

    ex-w01d03

---
+++ src/main/java/consultations/w01d03/Person.java
@@ -0,0 +1,9 @@
+package consultations.w01d03;
+
+public class Person {
+    public static void main(String[] args) {
+        System.out.println("Név: Teszt Elek");
+        System.out.println("Születési hely, idő: 1983.05.27.");
+        System.out.println("Anyja neve: Minta Gizella");
+    }
+}
