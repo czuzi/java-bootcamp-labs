6d2e5470f279bb0d7bd3fc7afee460a8902bf113
Istvan <palocsayi@gmail.com>
2022-07-29 16:40:20 +0200

    ex-w01d05 with scanner

--- src/main/java/consultations/w01d05/Geometry.java
+++ src/main/java/consultations/w01d05/Geometry.java
@@ -1,8 +1,17 @@
 package consultations.w01d05;
 
+import java.util.Scanner;
+
 public class Geometry {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(5, 8);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter first side: ");
+        int x = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("enter the other side: ");
+        int y = scanner.nextInt();
+        scanner.nextLine();
+        Rectangle rectangle = new Rectangle(x, y);
         System.out.println(rectangle.calculateArea());
     }
 }
