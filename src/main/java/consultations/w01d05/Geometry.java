24f1d57575579751ee6c77904c60786067b2de85
Istvan <palocsayi@gmail.com>
2022-07-29 16:33:42 +0200

    ex-w01d05

---
+++ src/main/java/consultations/w01d05/Geometry.java
@@ -0,0 +1,8 @@
+package consultations.w01d05;
+
+public class Geometry {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(5, 8);
+        System.out.println(rectangle.calculateArea());
+    }
+}
