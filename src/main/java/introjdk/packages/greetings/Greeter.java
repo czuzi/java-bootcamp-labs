c60103e86590ee7308ad44693a90083a84c4ba84
Istvan <palocsayi@gmail.com>
2022-08-03 00:36:23 +0200

    ex-statements

---
+++ src/main/java/introjdk/packages/greetings/Greeter.java
@@ -0,0 +1,8 @@
+package introjdk.packages.greetings;
+
+public class Greeter {
+
+    public String sayHello() {
+        return "hello world";
+    }
+}
---
+++ src/main/java/introjdk/packages/main/MainProgram.java
@@ -0,0 +1,10 @@
+package introjdk.packages.main;
+
+import introjdk.packages.greetings.Greeter;
+
+public class MainProgram {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
