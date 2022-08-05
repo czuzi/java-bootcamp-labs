3fd4beef9a296472fac8ff0547fcbb37e1ae63db
Istvan <palocsayi@gmail.com>
2022-08-05 07:17:10 +0200

    ex-linebreak

---
+++ src/main/java/linebreak/Car.java
@@ -0,0 +1,13 @@
+package linebreak;
+
+public class Car {
+
+    public String getBrandAndTypeInSeparateLines(String brand, String type) {
+        return brand + System.lineSeparator() + type;
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car();
+        System.out.println(car.getBrandAndTypeInSeparateLines("Volvo", "S40"));
+    }
+}
---
+++ src/main/java/linebreak/CityNames.java
@@ -0,0 +1,8 @@
+package linebreak;
+
+public class CityNames {
+
+    public static void main(String[] args) {
+        System.out.println("Debrecen" + System.lineSeparator() + "Budapest" + System.lineSeparator() + "Nyiregyhaza");
+    }
+}
