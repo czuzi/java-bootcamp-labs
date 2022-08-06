7c566fdd323fa65002a4d4b34c4b8d8f6beffc8c
Istvan <palocsayi@gmail.com>
2022-08-07 01:51:33 +0200

    ex-introcontrol

--- src/main/java/introcontrol/UserMenu.java
+++ src/main/java/introcontrol/UserMenu.java
@@ -6,9 +6,10 @@ public class UserMenu {
 
     public static void main(String[] args) {
 
-        System.out.println("1. Felhasznalok listazasa\n" +
-                            "2. Felhasznalo felvetele\n" +
-                            "Tobbi: Kilepes");
+        System.out.println("""
+                1. Felhasznalok listazasa
+                2. Felhasznalo felvetele
+                Tobbi: Kilepes""");
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of the option you choose: ");
         int input = scanner.nextInt();
