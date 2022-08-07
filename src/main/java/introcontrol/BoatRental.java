8aca998db1fef0e34ef6f27609d582409d7f4c76
Istvan <palocsayi@gmail.com>
2022-08-07 19:12:33 +0200

    ex-introcontrol boatrental

--- src/main/java/introcontrol/BoatRental.java
+++ src/main/java/introcontrol/BoatRental.java
@@ -5,14 +5,34 @@ import java.util.Scanner;
 public class BoatRental {
 
     public static void main(String[] args) {
-        int fiveSeatBoat = 5;
-        int threeSeatBoat = 3;
-        int twoSeatBoat = 2;
+        int fiveSeatBoat = 0;
+        int threeSeatBoat = 0;
+        int twoSeatBoat = 0;
 
         Scanner sc = new Scanner(System.in);
         int group = sc.nextInt();
         sc.nextLine();
 
+        int remaining = group;
+
+        if (remaining >= 4) {
+            remaining -= 5;
+            fiveSeatBoat++;
+        }
+        if (remaining >= 3) {
+            remaining -= 3;
+            threeSeatBoat++;
+        }
+        if (remaining > 0) {
+            remaining -= 2;
+            twoSeatBoat++;
+        }
+
+        System.out.println(fiveSeatBoat + "db ot szemelyes, " + threeSeatBoat + "db haromszemelyes es " + twoSeatBoat + "db ketszemelyes csonakot vittek el.");
+
+        if (remaining > 0) {
+            System.out.println(remaining + " szemely maradt a parton.");
+        }
     }
 
 
