90f3e1bb35b2576c2f2e775460d23a9328f14b9f
Istvan <palocsayi@gmail.com>
2022-08-10 20:56:34 +0200

    ex-debug

---
+++ src/main/java/debug/Debug.java
@@ -0,0 +1,29 @@
+package debug;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Debug {
+
+	public static void main(String[] args) {
+		List<String> fruits = Arrays.asList("alma", "körte", "barack", "szilva");
+
+		String peach = fruits.get(2);
+		fruits.set(3, peach);
+		for (int i = 0; i < fruits.size(); i++) {
+			fruits.set(i, fruits.get(i).substring(0, 2));
+
+		}
+		System.out.println(fruits.get(2));
+
+		int[] numbers = {3, 7, -2, 1, -4, 5};
+
+		numbers[2] = -10;
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[2]++;
+			numbers[2]++;
+		}
+
+		System.out.println(numbers[2]);
+	}
+}
