ad3ae511b9d8998b46c889ae3e57b3f3db304eb3
Istvan <palocsayi@gmail.com>
2022-08-15 20:46:28 +0200

    ex-w03d04

---
+++ src/main/java/consultations/w03d04/Stock.java
@@ -0,0 +1,26 @@
+package consultations.w03d04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stock {
+
+	List<Double> numbers;
+	public Stock(List<Double> numbers) {
+		this.numbers = numbers;
+	}
+
+	public double maxProfit() {
+		double maxProfit = 0;
+
+		for (int i = 0; i < numbers.size(); i++) {
+			for (int j = i+1; j < numbers.size(); j++) {
+				double actualProfit = numbers.get(j) - numbers.get(i);
+				if (actualProfit > maxProfit) {
+					maxProfit = actualProfit;
+				}
+			}
+		}
+		return maxProfit;
+	}
+}
