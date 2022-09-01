f0b6c1b20e15e724efe7cddf74e2ebb91898a304
Istvan <palocsayi@gmail.com>
2022-09-01 17:28:41 +0200

    ex-w06d04

---
+++ consultation/week06/src/main/java/day04/CourierFileManager.java
@@ -0,0 +1,27 @@
+package day04;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CourierFileManager {
+
+
+	public Courier createCourierByFile(Path path) {
+		List<String> lines;
+		Courier courier = new Courier();
+		try {
+			lines = Files.readAllLines(path);
+		} catch(IOException ioe) {
+			throw new IllegalStateException("Cant read the file", ioe);
+		}
+		for (String actualLine: lines) {
+			String[] temp = actualLine.split(" ");
+			Ride ride = new Ride(Integer.parseInt(temp[0]), Integer.parseInt(temp[1]), Integer.parseInt(temp[2]));
+			courier.addRide(ride);
+		}
+		return courier;
+	}
+}
