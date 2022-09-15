cf4ec9c2e080569e7d7d430e414b0027ba1d9e99
Istvan <palocsayi@gmail.com>
2022-09-15 09:07:23 +0200

    ex-algorithmsdecision

--- training-solutions/src/main/java/algorithmsdecision/towns/Town.java
+++ training-solutions/src/main/java/algorithmsdecision/towns/Town.java
@@ -5,12 +5,11 @@ import java.util.List;
 public class Town {
 
 	public boolean containsFewerHabitants(List<Integer> populations, int margin) {
-		boolean res = false;
 		for (int current : populations) {
 			if (current < margin) {
-				res = true;
+				return true;
 			}
 		}
-		return res;
+		return false;
 	}
 }
