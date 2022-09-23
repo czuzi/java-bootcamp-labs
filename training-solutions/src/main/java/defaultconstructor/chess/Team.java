95b9cacc7bddaf83412dd7b3c08e9beda74797f4
Istvan <palocsayi@gmail.com>
2022-09-23 17:38:50 +0200

    ex-defaultconstructor

---
+++ training-solutions/src/main/java/defaultconstructor/chess/Team.java
@@ -0,0 +1,25 @@
+package defaultconstructor.chess;
+
+import java.util.List;
+
+public class Team {
+
+	private Player playerOne;
+	private Player playerTwo;
+
+	public Player getPlayerOne() {
+		return playerOne;
+	}
+
+	public void setPlayerOne(Player playerOne) {
+		this.playerOne = playerOne;
+	}
+
+	public Player getPlayerTwo() {
+		return playerTwo;
+	}
+
+	public void setPlayerTwo(Player playerTwo) {
+		this.playerTwo = playerTwo;
+	}
+}
