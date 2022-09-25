ff38306eb2387529a52a964e3e6b6fa235323448
Istvan <palocsayi@gmail.com>
2022-09-25 19:20:04 +0200

    ex-inheritancemethods

---
+++ training-solutions/src/main/java/inheritancemethods/employee/BigBoss.java
@@ -0,0 +1,16 @@
+package inheritancemethods.employee;
+
+public class BigBoss extends Boss {
+
+	private double bonus;
+
+	public BigBoss(String name, String address, double salary, int numberOfEmployees, double bonus) {
+		super(name, address, salary, numberOfEmployees);
+		this.bonus = bonus;
+	}
+
+	@Override
+	public double getSalary() {
+		return super.getSalary() + bonus;
+	}
+}
