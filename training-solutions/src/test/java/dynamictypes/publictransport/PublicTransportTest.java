32ca9a402e1a59c29a284263639be51bf09b2730
Istvan <palocsayi@gmail.com>
2022-09-25 15:29:59 +0200

    ex-dynamictypes

--- training-solutions/src/test/java/dynamictypes/publictransport/PublicTransportTest.java
+++ training-solutions/src/test/java/dynamictypes/publictransport/PublicTransportTest.java
@@ -2,9 +2,6 @@ package dynamictypes.publictransport;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class PublicTransportTest {
