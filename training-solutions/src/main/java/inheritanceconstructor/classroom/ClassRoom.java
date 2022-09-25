adc5d699cf1c3996666f7efb8eef9ef6423a49f9
Istvan <palocsayi@gmail.com>
2022-09-25 14:08:52 +0200

    ex-inheritanceconstructor

---
+++ training-solutions/src/main/java/inheritanceconstructor/classroom/ClassRoom.java
@@ -0,0 +1,22 @@
+package inheritanceconstructor.classroom;
+
+public class ClassRoom extends Room {
+
+	private Facility facility;
+
+	public ClassRoom(String location, int capacity, Facility facility) {
+		super(location, capacity);
+		this.facility = facility;
+	}
+
+	public Facility getFacility() {
+		return facility;
+	}
+
+	public boolean isSuitable(Course course) {
+		if (this.facility != course.getFacilityNeeded() || this.getCapacity() < course.getParticipants()) {
+			return false;
+		}
+		return true;
+	}
+}
