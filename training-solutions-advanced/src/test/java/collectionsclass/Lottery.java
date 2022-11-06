c1f089cb0b0713f1ca458ee5365c2bce5e6e86b4
Istvan <palocsayi@gmail.com>
2022-11-06 23:25:00 +0100

    ex-collectionsclass

---
+++ training-solutions-advanced/src/test/java/collectionsclass/Lottery.java
@@ -0,0 +1,34 @@
+package collectionsclass;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lottery {
+
+	public List<Integer> selectWinningNumbers(int lotteryType, int poolSize) {
+		if (lotteryType >= poolSize) {
+			throw new IllegalArgumentException("There must be more balls than the winning numbers!");
+		}
+		List<Integer> pool = createNumberPool(poolSize);
+		return drawNumbers(lotteryType, pool);
+	}
+
+	private List<Integer> drawNumbers(int lotteryType, List<Integer> pool) {
+		List<Integer> winningNumbers = new ArrayList<>();
+		Collections.shuffle(winningNumbers);
+		for (int i = 0; i < lotteryType; i++) {
+			winningNumbers.add(pool.get(i));
+		}
+		return winningNumbers;
+	}
+
+	private List<Integer> createNumberPool(int poolSize) {
+		List<Integer> pool = new ArrayList<>();
+		for (int i = 0; i < poolSize; i++) {
+			pool.add(i);
+		}
+		return pool;
+	}
+
+}
