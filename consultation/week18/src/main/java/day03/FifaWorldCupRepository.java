f2b1b262271510441f5049b8fbb22841a2275c2d
Istvan <palocsayi@gmail.com>
2022-12-08 22:06:17 +0100

    ex-w18d05

--- consultation/week18/src/main/java/day03/FifaWorldCupRepository.java
+++ consultation/week18/src/main/java/day03/FifaWorldCupRepository.java
@@ -4,6 +4,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -15,6 +17,17 @@ public class FifaWorldCupRepository {
 		jdbcTemplate = new JdbcTemplate(ds);
 	}
 
+	private static Game mapRow(ResultSet rs, int rowNum) throws SQLException {
+		return new Game(
+				rs.getLong("id"),
+				rs.getDate("date_of_game").toLocalDate(),
+				rs.getString("first_team"),
+				rs.getString("second_team"),
+				rs.getInt("first_team_score"),
+				rs.getInt("second_team_score")
+		);
+	}
+
 	public void insertGame(Game game) {
 		jdbcTemplate.update("insert into fifa_world_cup (date_of_game, first_team, second_team, first_team_score, second_team_score) values (?, ?, ?, ?, ?)",
 				game.getDate(), game.getFirstTeam(), game.getSecondTeam(), game.getFirstTeamScore(), game.getSecondTeamScore());
@@ -22,26 +35,12 @@ public class FifaWorldCupRepository {
 
 	public List<Game> listAllGames() {
 		return jdbcTemplate.query("select * from fifa_world_cup",
-				((rs, rowNum) -> new Game(
-						rs.getLong("id"),
-						rs.getDate("date_of_game").toLocalDate(),
-						rs.getString("first_team"),
-						rs.getString("second_team"),
-						rs.getInt("first_team_score"),
-						rs.getInt("second_team_score")
-						)));
+				(FifaWorldCupRepository::mapRow));
 	}
 
 	public List<Game> listAllGamesOnGivenDate(LocalDate date) {
 		return jdbcTemplate.query("select * from fifa_world_cup where date_of_game = ?",
-				((rs, rowNum) -> new Game(
-						rs.getLong("id"),
-						rs.getDate("date_of_game").toLocalDate(),
-						rs.getString("first_team"),
-						rs.getString("second_team"),
-						rs.getInt("first_team_score"),
-						rs.getInt("second_team_score")
-				)), Date.valueOf(date));
+				(FifaWorldCupRepository::mapRow), Date.valueOf(date));
 	}
 
 	public Integer findMostGoalsInOneGame() {
@@ -51,14 +50,7 @@ public class FifaWorldCupRepository {
 
 	public Game findGameWithMostGoals() {
 		return jdbcTemplate.queryForObject("SELECT *, MAX(first_team_score + second_team_score) FROM fifa_world_cup;",
-				((rs, rowNum) -> new Game(
-				rs.getLong("id"),
-				rs.getDate("date_of_game").toLocalDate(),
-				rs.getString("first_team"),
-				rs.getString("second_team"),
-				rs.getInt("first_team_score"),
-				rs.getInt("second_team_score")
-		)));
+				(FifaWorldCupRepository::mapRow));
 	}
 
 	public List<String> findAllTeamWhichHasPlayed() {
